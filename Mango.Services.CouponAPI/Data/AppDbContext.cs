d65018f049a66078315f58bfd0ffdbcdafe99b24
Vadym <vadimmm972@gmail.com>
2024-02-06 21:24:27 +0200

    Adding a project to github

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+
+        public DbSet<Coupon> Couposn { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData( new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10OFF",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20OFF",
+                DiscountAmount = 20,
+                MinAmount = 40
+            });
+        }
+    }
+}
